9b9c601db3feba0d268ccf70d251f127a04e565d
Abdullah <38455373+abd1121@users.noreply.github.com>
2018-04-24 01:02:20 +0500

    Add files via upload
    Abdullah

---
+++ OOAD Final Project/register_user_diet.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace OOAD_Final_Project
+{
+    partial class register_user_diet
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.top_container = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.relatedSuggetionsContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // top_container
+            // 
+            this.top_container.Location = new System.Drawing.Point(15, 57);
+            this.top_container.Name = "top_container";
+            this.top_container.Size = new System.Drawing.Size(597, 137);
+            this.top_container.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 16F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(10, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(569, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hi, Muhammad Numair You Should Have This Today";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Related Suggetions";
+            // 
+            // relatedSuggetionsContainer
+            // 
+            this.relatedSuggetionsContainer.Location = new System.Drawing.Point(13, 237);
+            this.relatedSuggetionsContainer.Name = "relatedSuggetionsContainer";
+            this.relatedSuggetionsContainer.Size = new System.Drawing.Size(599, 144);
+            this.relatedSuggetionsContainer.TabIndex = 5;
+            // 
+            // register_user_diet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.relatedSuggetionsContainer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.top_container);
+            this.Name = "register_user_diet";
+            this.Size = new System.Drawing.Size(633, 409);
+            this.Load += new System.EventHandler(this.register_user_diet_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel top_container;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel relatedSuggetionsContainer;
+    }
+}
---
+++ OOAD Final Project/register_user_diet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOAD_Final_Project
+{
+    public partial class register_user_diet : UserControl
+    {
+        public register_user_diet()
+        {
+            InitializeComponent();
+            search_top_grid s = new search_top_grid();
+            top_container.Controls.Add(s);
+            for (int i = 0; i < 4; ++i)
+            {
+                search_bottom_grid sb = new search_bottom_grid();
+                relatedSuggetionsContainer.Controls.Add(sb);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void register_user_diet_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Hi, " + myUtil.loggedIN.Name + " You Should Have This Today";
+        }
+    }
+}
